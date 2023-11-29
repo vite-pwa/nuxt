01a88e7d8b4500750ed49f41a8f5269fbfb1ca69
userquin <userquin@gmail.com>
2023-11-29 20:32:57 +0100

    chore: module.cjs doesn't need patch

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -1,18 +1,6 @@
 import { readFileSync, writeFileSync } from 'node:fs'
 import { resolve } from 'node:path'
 
-// fix cjs export default
-const cjsModule = resolve('dist/module.cjs')
-const cjsModuleContent = readFileSync(cjsModule, 'utf-8')
-writeFileSync(
-  cjsModule,
-  cjsModuleContent.replace(
-    'module.exports = function',
-    'exports.default = function',
-  ),
-  'utf-8',
-)
-
 // fix d.mts imports
 const mDtsModule = resolve('dist/types.d.mts')
 const mDtsModuleContent = readFileSync(mDtsModule, 'utf-8')
