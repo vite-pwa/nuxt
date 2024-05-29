1c6a2356ce3f069958a034f982d5b2ac65577b98
Joaquín Sánchez <userquin@gmail.com>
2024-05-29 19:25:07 +0200

    fix!: use vite root instead nuxt root to resolve pwa assets (#138)
    * fix: use vite root instead nuxt root to resolve pwa assets
    
    * chore: update hint

--- src/utils/pwa-icons.ts
+++ src/utils/pwa-icons.ts
@@ -20,7 +20,8 @@ export async function preparePWAIconTypes(
   if (!configuration || configuration.disabled)
     return
 
-  const root = nuxt.options.rootDir ?? process.cwd()
+  // use vite root: pwa plugin using vite root, nuxt will configure vite root properly
+  const root = nuxt.options.vite.root ?? process.cwd()
   const { config, sources } = await loadConfiguration(root, configuration)
   if (!config.preset)
     return
