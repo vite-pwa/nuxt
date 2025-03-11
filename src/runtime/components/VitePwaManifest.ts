19164f2f3bb003ef4ef1d2d0d6cd190d3cbd29d3
david <dbzx-10299@hotmail.com>
2025-03-11 10:44:24 +0100

    fix: use useHead directly for generating link rel manifest

--- src/runtime/components/VitePwaManifest.ts
+++ src/runtime/components/VitePwaManifest.ts
@@ -6,23 +6,24 @@ import { useHead } from '#imports'
 export default defineComponent({
   async setup() {
     if (pwaInfo) {
-      const meta = ref<MetaObject>({ link: [] })
-      useHead(meta)
-
       const { webManifest } = pwaInfo
       if (webManifest) {
         const { href, useCredentials } = webManifest
         if (useCredentials) {
-          meta.value.link!.push({
-            rel: 'manifest',
-            href,
-            crossorigin: 'use-credentials',
+          useHead({
+            link: {
+              rel: 'manifest',
+              href,
+              crossorigin: 'use-credentials',
+            }
           })
         }
         else {
-          meta.value.link!.push({
-            rel: 'manifest',
-            href,
+          useHead({
+            link: {
+              rel: 'manifest',
+              href
+            }
           })
         }
       }
