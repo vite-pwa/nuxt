a79e175f101c584b4e74ddbce66d8294757b5a87
userquin <userquin@gmail.com>
2025-05-15 15:13:45 +0200

    feat(generateSW): add experimental payload json file runtime caching

--- src/types.ts
+++ src/types.ts
@@ -22,6 +22,43 @@ export interface ClientOptions {
 }
 
 export interface PwaModuleOptions extends Partial<VitePWAOptions> {
+  /**
+   * Experimental features.
+   */
+  experimental?: {
+    /**
+     * NOTE: this option will be ignored if using `injectManifest` strategy or when Nuxt experimental payload extraction
+     * is disabled.
+     *
+     * Enable custom runtime caching to resolve the payload.json requests with query parameters:
+     * - Workbox doesn't allow to configure `precacheAndRoute` `urlManipulation` option when using the `generateSW` strategy.
+     * - Nuxt SSG will generate a payload.json file and will fetch it with a query parameter.
+     * - The service worker cannot resolve the payload.json request with query parameters, and you won't get the payload.
+     *
+     * Enabling this option will add a custom runtime caching handler to the service worker to resolve the payload files
+     * with query parameters.
+     *
+     * If you're using `injectManifest` strategy, you can fix the issue in your custom service worker adding the
+     * following `urlManipulation` callback to the `precacheAndRouter` call:
+     * ```ts
+     * // self.__WB_MANIFEST is the default injection point
+     * precacheAndRoute(
+     *   self.__WB_MANIFEST,
+     *   {
+     *     urlManipulation: ({ url }) => {
+     *       const urls: URL[] = []
+     *       if (url.pathname.endsWith('_payload.json')) {
+     *         const newUrl = new URL(url.href)
+     *         newUrl.search = ''
+     *         urls.push(newUrl)
+     *      }
+     *      return urls
+     *    }
+     *  }
+     * )
+     */
+    enableWorkboxPayloadQueryParams?: true
+  }
   registerWebManifestInRouteRules?: boolean
   /**
    * Writes the plugin to disk: defaults to false (debug).
